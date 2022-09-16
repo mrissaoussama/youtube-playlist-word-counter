0179075cb91f1afe50022f3b5ae87388ac8bb0a5
oussamamrissa@mahdia.r-iset.tn <oussamamrissa@mahdia.r-iset.tn>
2022-09-16 15:52:03 +0100

    Add project files.

---
+++ youtube channel most used words in titles/Program.cs
@@ -0,0 +1,30 @@
+﻿using YoutubeExplode;
+var youtube = new YoutubeClient();
+var list=new List<string>();
+Console.WriteLine("input list url");
+
+var playlisturl =Console.ReadLine();
+Console.WriteLine("loading...");
+Dictionary<string,int> result=new(); 
+await foreach (var video in youtube.Playlists.GetVideosAsync(
+    playlisturl
+))
+{
+  list.Add(video.Title);
+}
+foreach(var sentence in list)
+{
+    var words=sentence.Split(' ');
+    foreach(var word in words)
+    {
+        if (result.TryAdd(word, 1) == false)
+            result[word] += 1;
+    }
+}
+var sortedDict = from entry in result orderby entry.Value ascending select entry ;
+
+foreach (KeyValuePair<string,int> kvp in sortedDict)
+{
+    Console.WriteLine("word = {0}, count = {1}", kvp.Key, kvp.Value);
+}
+Console.WriteLine("total videos = {0}, total words = {1}", list.Count,sortedDict.Count());
